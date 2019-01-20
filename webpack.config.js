70656764a20ed233e18791e5a7e1bc1b55e62607
Gavin Quirk <gavinquirk@gmail.com>
2019-01-20 14:29:15 -0800

    rules for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
+const autoprefixer = require ('autoprefixer')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: './src/index.js',
+  devtool: 'cheap-module-eval-source-map', // Enable dev tools
+  entry: './src/index.js', // File to begin with
   output: {
     path: '',
-    filename: 'bundle.js',
-    publicPath: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js', // Bundled file to be created
+    publicPath: path.resolve(__dirname, 'dist') // Current Directory / dist
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -14,9 +15,38 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
+        test: /\.js$/, // Test for files with this extension
+        loader: 'babel-loader', // Use this loader
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [ // Use with multiple loaders or config -- loaded from bottom to top
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader', // Ability to use css
+            options: { // Configurations
+              importLoaders: 1, // Must tell css loader that postcss-loader is being used
+              modules: true, // Enable CSS modules
+              localIdentname: '[name]__[local]__[hash:base64:5]' // Class name __ Component Name __ Hash
+            }
+          },
+          {
+            loader: 'postcss-loader', // Could be configured for sass
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({ // Adds browser specific prefixes to css
+                  browsers: [
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                })
+              ]
+            }
+          }
+        ]
       }
     ]
   }
