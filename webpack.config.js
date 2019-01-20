a7ec1b873e85c0796af5404e5944ba9495c4c2ed
Gavin Quirk <gavinquirk@gmail.com>
2019-01-20 14:15:57 -0800

    webpack config, babel for react

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  output: {
+    path: '',
+    filename: 'bundle.js',
+    publicPath: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
